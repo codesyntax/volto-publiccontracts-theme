99498e5d970fdf203df57ff45cb1ec1b1968beeb
Lur Ibargutxi <libargutxi@codesyntax.com>
2022-06-08 12:10:40 +0200

    stylelint and contract.less new file

--- src/Components/Widgets/DatesTableWidget.jsx
+++ src/Components/Widgets/DatesTableWidget.jsx
@@ -2,43 +2,40 @@ import React from 'react';
 import { ReactTableWidget } from '@eeacms/volto-react-table-widget';
 
 const ItemSchema = () => ({
-    title: 'Dates',
-    properties: {
-        title: {
-            title: 'Title',
-            description: 'Enter the title of this date.',
-            type: 'string',
-        },
-        date: {
-            title: 'Date',
-            description: 'Enter the date',
-            type: 'date',
-        }
+  title: 'Dates',
+  properties: {
+    title: {
+      title: 'Title',
+      description: 'Enter the title of this date.',
+      type: 'string',
     },
-    fieldsets: [
-        {
-            id: 'default',
-            title: 'Dates',
-            fields: [
-                'title',
-                'date'
-            ],
-        },
-    ],
-    required: [],
+    date: {
+      title: 'Date',
+      description: 'Enter the date',
+      type: 'date',
+    },
+  },
+  fieldsets: [
+    {
+      id: 'default',
+      title: 'Dates',
+      fields: ['title', 'date'],
+    },
+  ],
+  required: [],
 });
 
 const DatesTableWidget = (props) => {
-    return (
-        <ReactTableWidget
-            schema={ItemSchema()}
-            {...props}
-            csvexport={false}
-            csvimport={false}
-            value={props.value?.items || props.default?.items || []}
-            onChange={(id, value) => props.onChange(id, { items: value })}
-        />
-    );
+  return (
+    <ReactTableWidget
+      schema={ItemSchema()}
+      {...props}
+      csvexport={false}
+      csvimport={false}
+      value={props.value?.items || props.default?.items || []}
+      onChange={(id, value) => props.onChange(id, { items: value })}
+    />
+  );
 };
 
 export default DatesTableWidget;
